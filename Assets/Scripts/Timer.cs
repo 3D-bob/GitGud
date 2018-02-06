ebb0ceccbea5d11c7ba3b8e1718a9e909ed02afe
3D-bob <tumi.ahonen@dnainternet.net>
2018-02-06 14:28:53 +0200

    Grephics

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+    public Text timer;
+    float time;
+    int timeInt;
+
+    void Start()
+    {
+        time = 0;
+        UpdateTimer();     
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        UpdateTimer();
+        timeInt = Mathf.RoundToInt(time);
+    }
+
+    void UpdateTimer()
+    {
+        timer.text = "Time: " + timeInt.ToString();
+    }
+}
