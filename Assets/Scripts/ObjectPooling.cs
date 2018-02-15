5860433c84906d39e0b8bc56295f1118839aeae8
JoonaJappinen <jappinen.joona@gmail.com>
2018-02-15 17:52:15 +0200

    ObjectPooling Proof of Concept

---
+++ Assets/Scripts/ObjectPooling.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooling : MonoBehaviour {
+
+    public List<GameObject> pooledObjects;
+    public GameObject objectToPool;
+    public int poolSize;
+
+	// Use this for initialization
+	void Start ()
+    {
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject obj = (GameObject)Instantiate(objectToPool);
+            obj.SetActive(false);
+            pooledObjects.Add(obj);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            GameObject bullet = ReturnFromPool();
+
+            if (bullet != null)
+            {
+                bullet.transform.position = this.transform.position;
+                bullet.transform.rotation = this.transform.rotation;
+                bullet.SetActive(true);
+            }
+        }
+
+    }
+
+    public GameObject ReturnFromPool()
+    {
+        for (int i = 0; i < pooledObjects.Count; i++)
+        {
+            if(!pooledObjects[i].activeInHierarchy)
+            {
+                return pooledObjects[i];
+            }
+        }
+        return null;
+    }
+}
---
+++ Assets/testProj.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testProj : MonoBehaviour {
+
+	void OnEnable ()
+    {
+        StartCoroutine(LifeTime());
+	}
+	
+	void Update ()
+    {
+        transform.Translate(00000.1f, 0, 0 * Time.deltaTime);
+                
+	}
+
+    IEnumerator LifeTime()
+    {
+       yield return new WaitForSeconds(2);
+       gameObject.SetActive(false);
+    }
+}
